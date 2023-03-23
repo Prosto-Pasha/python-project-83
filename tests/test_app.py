96a9ec57998e4e5726d0d488cb5257fc60e0bcc8
Pavel Belyavskiy <prostopasha@gmail.com>
2023-03-23 22:00:47 +0200

    Add blank test

---
+++ tests/test_app.py
@@ -0,0 +1,40 @@
+import pytest
+from page_analyzer.app import app
+# from werkzeug.test import Client
+# from werkzeug.testapp import test_app
+
+
+@pytest.fixture()
+def app_fixture():
+    app.config.update({"TESTING": True, })
+
+    # other setup can go here
+
+    yield app
+
+    # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(app_fixture):
+    return app_fixture.test_client()
+
+
+@pytest.fixture()
+def runner(app_fixture):
+    return app_fixture.test_cli_runner()
+
+
+# @pytest.fixture()
+# def client():
+#    return Client(test_app)
+
+
+# def test_request(client):
+#    response = client.get("/")
+#    assert response.status_code == 200
+#    assert b"<h2>Hello, World!</h2>" in response.data
+
+
+def test_test():
+    assert 1 == True
