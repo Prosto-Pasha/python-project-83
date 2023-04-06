d4cd2d87c076240cd2a07dad4d9df5b19a8debae
Pavel Belyavskiy <prostopasha@gmail.com>
2023-04-06 17:18:27 +0300

    Refactor test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -54,8 +54,3 @@ def test_get_parsed_url():
 #    response = client.get("/")
 #    assert response.status_code == 200
 #    assert b"<h2>Hello, World!</h2>" in response.data
-
-
-def test_test():
-    # TODO Нужно добавить тесты
-    assert True
